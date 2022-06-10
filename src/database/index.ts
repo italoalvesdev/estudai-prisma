db8e318daafc1a3647a5e4a4f2497e3aee55d0f6
Italo Alves <italo_silva007@hotmail.com>
2022-06-10 19:40:43 -0300

    Deletion of files due to orm change

--- src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import { AppDataSource } from '../data-source'
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been iniatialized!')
-  })
-  .catch(err => {
-    console.error('Error during Data Source iniatiazation', err)
-  })
