1652d3d01b5633c0e1a12ccd7e479f6a5fd6f789
Italo Alves <italo_silva007@hotmail.com>
2022-07-29 19:04:04 -0300

    feat: creating more environment variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,8 @@
 export default {
   port: process.env.PORT || 3333,
-  jwtSecret: process.env.JWT_SECRET || 'batatinha0011@@'
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || '829f0400c0b07711411bb78ff65bba1b',
+  expiresInAccessToken: '60s',
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || 'f770b3eee79bf1a0b0fd31310277f21c',
+  expiresInRefreshToken: '30d',
+  expiresInRefreshTokenDays: 30
 }
