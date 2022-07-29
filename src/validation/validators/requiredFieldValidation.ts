ad4b6f00ecfe62ae7424d5d05d6b9a30067132b8
Italo Alves <italo_silva007@hotmail.com>
2022-07-29 01:55:52 -0300

    feat: add fields validations flies

---
+++ src/validation/validators/requiredFieldValidation.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from "../../presentation/errors/missingParamError";
+import { Validation } from "../../presentation/protocols/validation";
+
+export class RequiredFieldValidation implements Validation {
+  constructor(
+    private readonly fieldName: string
+  ) {}
+
+  validate(input: any): Error {
+    if(!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
+  }
+}
---
+++ src/validation/validators/validationComposite.ts
@@ -0,0 +1,16 @@
+import { Validation } from "../../presentation/protocols/validation";
+
+export class ValidationComposite implements Validation {
+  constructor(
+    private readonly validator: Validation[]
+  ) {}
+
+  validate(input: any): Error {
+    for(const validation of this.validator) {
+      const error = validation.validate(input);
+      if(error) {
+        return error;
+      }
+    }
+  }
+}
