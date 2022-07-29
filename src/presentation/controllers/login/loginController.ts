064d1993196ede7b5f7e9c76052f394de9338adb
Italo Alves <italo_silva007@hotmail.com>
2022-07-29 02:31:32 -0300

    feat(presentation): add login controller and types export file

---
+++ src/presentation/controllers/login/loginController.ts
@@ -0,0 +1,34 @@
+import { badRequest, ok, serverError, unauthorized } from "../../helpers/http/httpHelper";
+import { Authentication, Controller, HttpRequest, HttpResponse, Validation } from "./loginControllerProtocols";
+
+export class LoginController implements Controller {
+  constructor(
+    private readonly authentication: Authentication,
+    private readonly validation: Validation,
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.body);
+
+      if(error) {
+        return badRequest(error);
+      }
+
+      const { email, password } = httpRequest.body;
+
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      });
+      
+      if(!accessToken) {
+        return unauthorized();
+      }
+
+      return ok({accessToken});
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
