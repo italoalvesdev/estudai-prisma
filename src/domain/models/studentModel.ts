fbe7fabf76b70fcf1f54d0870f396d4c33512688
Italo Alves <italo_silva007@hotmail.com>
2022-07-29 01:58:10 -0300

    feat: add student model

---
+++ src/domain/models/studentModel.ts
@@ -0,0 +1,18 @@
+export interface StudentModel {
+  id: string
+  fullName: string
+  nickName?: string
+  cpf: string
+  birthday: Date
+  postalCode: string
+  state: string
+  city: string
+  street: string
+  neighborhood: string
+  addressNumber: string
+  details?: string
+  foneMobile: string
+  foneHome?: string
+  email: string
+  password: string
+}
