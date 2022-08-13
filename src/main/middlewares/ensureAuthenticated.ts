415623485fbcf03709354f94a40643ab6f604164
Italo Alves <italo_silva007@hotmail.com>
2022-08-13 20:43:49 -0300

    bug: fix ensureAuthenticated middleware error handling

--- src/main/middlewares/ensureAuthenticated.ts
+++ src/main/middlewares/ensureAuthenticated.ts
@@ -1,30 +1,34 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from 'jsonwebtoken'
 import { PayloadData } from "../../useCases/protocols/criptography";
-
 import { JwtTokenMissingError } from "./errors/jwtTokenMissingError";
 import { JwtInvalidTokenError } from "./errors/jwtInvalidTokenError";
 import env from "../config/env";
-
+import dayjs from "dayjs";
 
 export async function ensureAuthenticated(
-  req: Request, 
-  res: Response, 
+  req: Request,
+  res: Response,
   next: NextFunction
-  ) {
-    const authHeader = req.headers.authorization;
+) {
+  const authHeader = req.headers.authorization;
 
+  try {
     if (!authHeader) {
-      throw new JwtTokenMissingError();
+      return res.status(401).json({ error: new JwtTokenMissingError().message });
     }
 
     const [, token] = authHeader.split(" ");
-
-    try {
-      verify(token, env.accessTokenSecret) as PayloadData;
-
-      next();
-    } catch (err) {
-      throw new JwtInvalidTokenError()
+    verify(token, env.accessTokenSecret) as PayloadData;
+    
+    
+    next();
+  } catch (err) {
+    switch (err.message) {
+      case 'jwt expired':
+        return res.status(401).json({ error: 'JWT token expired', expiredAt: err?.expiredAt  })
     }
+    console.log(err)
+    return res.status(401).json({ error: new JwtInvalidTokenError().message })
+  }
 }
--- src/useCases/protocols/criptography/decrypter.ts
+++ src/useCases/protocols/criptography/decrypter.ts
@@ -1,6 +1,7 @@
 export interface PayloadData {
   sub?: string
   email?: string
+  exp?: number
 }
 
 export interface Decrypter {
